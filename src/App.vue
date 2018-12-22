d1921b21387bba25b3a5cb58aa767c1ab08fadea
Roque Alarcon <roquealarcon363@gmail.com>
2018-12-22 00:00:42 -0500

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    section.section
+      nav.navbar.has-shadow
+        .container
+          input.input.is-large(type="text", placeholder="Buscar Canciones", v-model="searchQuery")
+          a.button.is-info.is-large(v-on:click="search") Buscar
+          a.button.is-danger.is-large &times;
+
+      .container.results
+        .columns
+          .cloumn(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinetta' },
+  { name: 'Hope', artist: 'The Chainsmokers' },
+  { name: 'Cemetery', artist: 'COIN' },
+  { name: 'Reforget', artist: 'Lauv' }
+]
 export default {
   name: 'app',
+
   data () {
     return {
-      msg: 'Hola mundo'
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+
+  methods: {
+    search () {
+      console.log(this.searchQuery)
+      this.tracks = tracks
     }
   }
 }
