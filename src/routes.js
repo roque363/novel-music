3f99d27a101d3ad9fecbdd9ff8273fe2b83b4ed9
Roque Alarcon <roquealarcon363@gmail.com>
2020-07-29 21:08:08 -0500

    :zap: Add constant for public path

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,18 @@ import About from './views/About.vue';
 import TrackDetail from './views/TrackDetail.vue';
 import NotFound from './views/NotFound.vue';
 
+const BASE_RUTE = '/novel-music';
+
 const routes = [
-  { path: '/', component: Search, name: 'search' },
-  { path: '/about', component: About, name: 'about' },
+  { path: BASE_RUTE + '/', component: Search, name: 'search' },
+  { path: BASE_RUTE + '/about', component: About, name: 'about' },
   {
-    path: '/track/:id/:slug',
+    path: BASE_RUTE + '/track/:id/:slug',
     component: TrackDetail,
     name: 'track',
   },
-  { path: '/404', component: NotFound },
-  { path: '/*', redirect: '/404' },
+  { path: BASE_RUTE + '/404', component: NotFound },
+  { path: '/*', redirect: BASE_RUTE + '/404' },
 ];
 
 export default routes;
